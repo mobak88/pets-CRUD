7dbbffeab4d6d9dc91244c0bc27376e6c297cccc
mobak88 <mostafa0401@ymail.com>
2022-10-28 23:56:46 +0200

    Creating CRUD operations with database

--- index.js
+++ index.js
@@ -9,15 +9,67 @@ app.use(express.json());
 
 // Create pet
 
-app.post('/pet', async (req, res) => {
+app.post('/pets', async (req, res) => {
     const { petName, petAge, ownerName, species } = req.body;
 
     console.log(req.body);
 
     try {
-        const newPet = await pool.query('INSERT INTO pets (pet_name, pet_age, owner_name, species) VALUES($1, $2, $3, $4) RETURNING *', [petName, petAge, ownerName, species]);
+        const newPet = await pool.query(
+            'INSERT INTO pets (pet_name, pet_age, owner_name, species) VALUES($1, $2, $3, $4) RETURNING *',
+            [petName, petAge, ownerName, species]
+        );
 
         res.json(newPet.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// Get all pets
+
+app.get('/pets', async (req, res) => {
+    try {
+        const allPets = await pool.query('SELECT * FROM pets');
+        res.json(allPets.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// Get a pet
+
+app.get('/pets/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const pet = await pool.query(
+            'SELECT * FROM pets WHERE pet_id = $1',
+            [id]
+        );
+        res.json(pet.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.put('/pets/:id', async (req, res) => {
+    const { id } = req.params;
+    const { petName, petAge, ownerName, species } = req.body;
+    try {
+        const updatePet = await pool.query(
+            'UPDATE pets SET pet_name=COALESCE($1, pet_name), pet_age=COALESCE($2, pet_age), owner_name=COALESCE($3, pet_age), species=COALESCE($4, species)  WHERE pet_id = $5',
+            [petName, petAge, ownerName, species, id]);
+        res.json(updatePet.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.delete('/pets/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletePet = await pool.query('DELETE FROM pets WHERE pet_id = $1', [id]);
+        res.json(`Pet with id ${id} was deleted from the database`);
     } catch (err) {
         console.error(err.message);
     }
